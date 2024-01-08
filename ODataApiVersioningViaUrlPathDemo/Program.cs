bdfa593e48ae52502f81742e96a0e19903da3c6d
Peter Kovecses <Peter_Kovecses@epam.com>
2024-01-08 16:00:10 +0100

    Add versioning support to Swagger

--- ODataApiVersioningViaUrlPathDemo/Program.cs
+++ ODataApiVersioningViaUrlPathDemo/Program.cs
@@ -1,5 +1,6 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore.OData;
+using ODataApiVersioningViaUrlPathDemo.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,7 @@ builder.Services
     .AddApiVersioning(options =>
     {
         options.ReportApiVersions = true;
-        options.DefaultApiVersion = new ApiVersion(1);
+        options.DefaultApiVersion = new ApiVersion(2);
         options.AssumeDefaultVersionWhenUnspecified = true;
     })
     .AddOData(options =>
@@ -18,11 +19,32 @@ builder.Services
     .AddODataApiExplorer(options =>
     {
         options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
     });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.OperationFilter<SwaggerDefaultValues>();
+
+    // Resolve Conflicting Actions (identical routes that differ only by HTTP method)
+    options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "API V1 Title",
+        Version = "V1",
+        Description = "API V1 Description",
+    });
+
+    options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "API V2 Title",
+        Version = "V2",
+        Description = "API V2 Description",
+    });
+});
 
 var app = builder.Build();
 
@@ -30,7 +52,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        var descriptions = app.DescribeApiVersions();
+
+        foreach (var description in descriptions)
+        {
+            var url = $"/swagger/{description.GroupName}/swagger.json";
+            var name = description.GroupName.ToUpperInvariant();
+            options.SwaggerEndpoint(url, name);
+        }
+    });
 }
 
 app.UseHttpsRedirection();
